cd5438abfc381a062c1f71001e03ee71a78f5b3a
TrueCopperR6 <86630968+TrueCopperR6@users.noreply.github.com>
2026-07-31 17:27:41 +0100

    Update checks.py

--- Eldobot-master/checks.py
+++ Eldobot-master/checks.py
@@ -14,11 +14,18 @@ def server_check(id, name):
         )
     id = str(id)
     if id in serversList:
-        for d, v in default.items():
-            if d in serversList[id]:
-                continue
-            else:
-                serversList[id][d] = v
+       import copy
+
+def merge_defaults(server, default):
+    for k, v in default.items():
+        if k not in server:
+            server[k] = copy.deepcopy(v)
+        elif isinstance(v, dict) and isinstance(server[k], dict):
+            merge_defaults(server[k], v)
+            if id in serversList:
+    merge_defaults(serversList[id], default)
+else:
+    serversList[id] = copy.deepcopy(default)
     else:
         serversList[id] = copy.deepcopy(default)
     serversList[id]['name'] = name
